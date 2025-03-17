f4a39209b48ea110d944a356cfde9ba0430aceab
zaherushka <sanmor.shop@gmail.com>
2025-03-17 16:32:44 +0300

    Initial Commit

---
+++ Fireline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fireline
+//
+//  Created by Захар Ячник on 17.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
